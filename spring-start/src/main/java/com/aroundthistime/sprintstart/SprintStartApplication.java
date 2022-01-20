37a84fddd80be7ded86ed3e069dbbf745b51df84
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2022-01-20 15:32:01 +0900

    http://start.spring.io/통해서 프로젝트 생성 완료

---
+++ spring-start/src/main/java/com/aroundthistime/sprintstart/SprintStartApplication.java
@@ -0,0 +1,13 @@
+package com.aroundthistime.sprintstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintStartApplication.class, args);
+	}
+
+}
---
+++ spring-start/src/test/java/com/aroundthistime/sprintstart/SprintStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aroundthistime.sprintstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
